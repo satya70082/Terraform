39632e8d3c4879da8e339e44b8ca1ab1ee8f47d0
satya70082 <146988493+satya70082@users.noreply.github.com>
2024-11-29 22:44:25 +0530

    add

--- nullresource/local-exec/db.sql
+++ nullresource/local-exec/db.sql
@@ -6,7 +6,7 @@ USE mydatabase;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL,city VARCHAR(255) NOT NULL,
 );
 -- Create a table for orders
 CREATE TABLE orders (
@@ -20,9 +20,10 @@ CREATE TABLE orders (
 
 -- Insert sample data into users table
 INSERT INTO users (name, email) VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com'),
-('Charlie', 'charlie@example.com');
+('Alice', 'alice@example.com' ,'New York'),
+('Bob', 'bob@example.com' ,'Los Angeles'),
+('Charlie', 'charlie@example.com' ,'Chicago')
+('David', 'david@gmail.com' ,'San Francisco');
 
 -- Insert sample data into orders table
 INSERT INTO orders (user_id, product_name, quantity) VALUES
